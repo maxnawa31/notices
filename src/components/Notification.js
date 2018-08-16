d6eec8cb6482d4f202791adcb4d7d8f68cca5964
maxnawa31 <mnawa31@gmail.com>
2018-08-16 15:17:29 -0700

    mapStateToProps working for notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -8,7 +8,7 @@ export default class Notification extends Component {
   render() {
     return (
       <div>
-
+      hello
       </div>
     )
   }
--- src/containers/Notification.js
+++ src/containers/Notification.js
@@ -2,8 +2,8 @@ import Notification from '../components/Notification';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
-  mode: state.mode,
-  color: state.color
+  mode: state.notifications.mode,
+  color: state.notifications.color
 })
 
 export default connect(mapStateToProps, null)(Notification);
