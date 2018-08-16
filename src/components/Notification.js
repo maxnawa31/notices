d30ff2343a497772ab5c5c4dc702466f7856622d
maxnawa31 <mnawa31@gmail.com>
2018-08-16 15:38:15 -0700

    notifications working with color

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -6,10 +6,27 @@ export default class Notification extends Component {
     message: false
   }
   render() {
+    const NotificationStyle = {
+      height: '100%',
+      width: '20%',
+      backgroundColor: this.props.color !== null ? `${this.props.color}` : 'white'
+    }
+
+    let notificationText;
+    if(this.props.color === 'grey') {
+      notificationText = `loading ${this.props.mode}`
+    }else if(this.props.color === 'green') {
+      notificationText = `Successfully loaded ${this.props.mode}`
+    }else if(this.props.color === 'red') {
+      notificationText = `Error loading ${this.props.mode}`
+    }else {
+      notificationText = ''
+    }
     return (
-      <div>
-      hello
+      <div style = {NotificationStyle} >
+      { notificationText }
       </div>
     )
   }
 }
+
--- src/containers/Notification.js
+++ src/containers/Notification.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
   mode: state.notifications.mode,
-  color: state.notifications.color
+  color: state.notifications.color,
+  isLoading: state.textTransform.isLoading
 })
 
 export default connect(mapStateToProps, null)(Notification);
